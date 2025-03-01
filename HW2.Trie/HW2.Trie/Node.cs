9c08304126fd3cbe57824ce89c29f203a4da8212
Bogdanova Arina <arinabogdanova1967@mail.ru>
2025-03-01 18:53:12 +0300

    на радость StyleCop-у. +комментарии

--- HW2.Trie/HW2.Trie/Node.cs
+++ HW2.Trie/HW2.Trie/Node.cs
@@ -3,12 +3,12 @@ namespace HW2.Trie;
 public class Node<T> (char symbol)
 {
     private char Symbol { get; } = symbol;
-    private Dictionary<char, Node<T>> SubNodes { get; } = new();
+    private Dictionary<char, Node<T>?> SubNodes { get; } = new();
     private bool IsWord { get; set; }
-
-    public Node<T> TryFind(char symbol)
+    
+    public Node<T>? TryFind(char symbol)
     {
-        if (SubNodes.TryGetValue(symbol, out Node<T> value))
+        if (SubNodes.TryGetValue(symbol, out Node<T>? value))
         {
             return value;
         }
@@ -18,16 +18,16 @@ public class Node<T> (char symbol)
         }
     }
     
-    public static void AddNode(string key, Node<T> node)
+    public static void AddNode(string key, Node<T>? node)
     {
         if (string.IsNullOrEmpty(key))
         {
-            node.IsWord = true;
+            if (node != null) node.IsWord = true;
         }
         else
         {
             var symbol = key[0];
-            var subnode = node.TryFind(symbol);
+            var subnode = node?.TryFind(symbol);
             if (subnode != null)
             {
                 AddNode(key[1..], subnode);
@@ -35,17 +35,17 @@ public class Node<T> (char symbol)
             else
             {
                 var newNode = new Node<T>(key[0]);
-                node.SubNodes.Add(key[0], newNode);
+                node?.SubNodes.Add(key[0], newNode);
                 AddNode(key.Substring(1), newNode);
             }
         }
     }
     
-    public static bool RemoveNode(string key, Node<T> node)
+    public static bool RemoveNode(string key, Node<T>? node)
     {
         if (string.IsNullOrEmpty(key))
         {
-            if (node.IsWord)
+            if (node != null && node.IsWord)
             {
                 node.IsWord = false;
                 return true;
@@ -53,7 +53,7 @@ public class Node<T> (char symbol)
             return false;
         }
         
-        var subnode = node.TryFind(key[0]);
+        var subnode = node?.TryFind(key[0]);
         if (subnode == null)
         {
             return false;
@@ -65,19 +65,19 @@ public class Node<T> (char symbol)
         {
             if (subnode.SubNodes.Count == 0 && !subnode.IsWord)
             {
-                node.SubNodes.Remove(key[0]);
+                node?.SubNodes.Remove(key[0]);
             }
         }
 
         return result;
     }
     
-    public static bool SearchNode(string key, Node<T> node)
+    public static bool SearchNode(string key, Node<T>? node)
     {
         var result = false;
         if (string.IsNullOrEmpty(key))
         {
-            if (node.IsWord)
+            if (node != null && node.IsWord)
             {
                 result = true;
                 return result;
@@ -85,7 +85,7 @@ public class Node<T> (char symbol)
         }
         else
         {
-            var subnode = node.TryFind(key[0]);
+            var subnode = node?.TryFind(key[0]);
             if (subnode != null)
             {
                 result = SearchNode(key.Substring(1), subnode);
@@ -95,21 +95,21 @@ public class Node<T> (char symbol)
         return result;
     }
     
-    public static int CountingWordsWithThisPrefix(String prefix, Node<T> node)
+    public static int CountingWordsWithThisPrefix(String prefix, Node<T>? node)
     {
         int result = 0;
         while (!string.IsNullOrEmpty(prefix))
         {
-            node = node.TryFind(prefix[0]);
+            node = node?.TryFind(prefix[0]);
             prefix = prefix.Substring(1);
         }
         
-        if (node.IsWord)
+        if (node != null && node.IsWord)
         {
             ++result;
         }
 
-        if (node.SubNodes.Count > 0)
+        if (node != null && node.SubNodes.Count > 0)
         {
             foreach (var keyValuePair in node.SubNodes)
             {
--- HW2.Trie/HW2.Trie/Trie.cs
+++ HW2.Trie/HW2.Trie/Trie.cs
@@ -1,21 +1,36 @@
 namespace HW2.Trie;
 
+/// <summary>
+/// This prefixTree.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class Trie<T>
 {
-    private Node<T> root;
+    private readonly Node<T>? _root;
+    /// <summary>
+    /// Gets the number of unique words in the tree
+    /// </summary>
     public int Size { get; set; }
 
+    /// <summary>
+    /// Trie
+    /// </summary>
     public Trie()
     {
-        root = new Node<T>('\0');
+        _root = new Node<T>('\0');
         Size = 0;
     }
 
+    /// <summary>
+    /// Adds a word to the tree
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>True if there was no such word in the tree.</returns>
     public bool Add(string key)
     {
         if (!Contains(key))
         {
-            Node<T>.AddNode(key, root); 
+            Node<T>.AddNode(key, _root); 
             ++Size;
             return true;
         }
@@ -23,9 +38,14 @@ public class Trie<T>
         return false;
     }
 
+    /// <summary>
+    /// Removes a word from the tree.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>True if such a word existed in the tree.</returns>
     public bool Remove(string key)
     {
-        bool result = Node<T>.RemoveNode(key, root);
+        bool result = Node<T>.RemoveNode(key, _root);
         if (result)
         {
             --Size;
@@ -33,13 +53,23 @@ public class Trie<T>
         return result;
     }
 
+    /// <summary>
+    /// Checks if there is such a word in the tree.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>True if such a word is contained in the tree.</returns>
     public bool Contains(string key)
     {
-        return Node<T>.SearchNode(key, root);
+        return Node<T>.SearchNode(key, _root);
     }
 
+    /// <summary>
+    /// Counts how many words in the tree begin with this prefix.
+    /// </summary>
+    /// <param name="prefix"></param>
+    /// <returns>Returns the number of words in the tree that start with this prefix.</returns>
     public int HowManyStartsWithPrefix(String prefix)
     {
-        return Node<T>.CountingWordsWithThisPrefix(prefix, root);
+        return Node<T>.CountingWordsWithThisPrefix(prefix, _root);
     }
 }
