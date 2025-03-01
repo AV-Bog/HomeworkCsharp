046fc0577612d5b3604c4bf616e58c55e772aef3
Bogdanova Arina <arinabogdanova1967@mail.ru>
2025-03-01 16:28:49 +0300

    правки (кроме словаря)

--- HW2.Trie/HW2.Trie/Node.cs
+++ HW2.Trie/HW2.Trie/Node.cs
@@ -1,18 +1,12 @@
 namespace HW2.Trie;
 
-public class Node<T>
+public class Node<T> (char symbol)
 {
-    public char Symbol { get; set; }
-    public bool IsWord { get; set; }
-    public Dictionary<char, Node<T>> SubNodes { get; set; }
-    
-    public Node(char symbol)
-    {
-        Symbol = symbol;
-        SubNodes = new Dictionary<char, Node<T>>();
-    }
+    private char Symbol { get; } = symbol;
+    private Dictionary<char, Node<T>> SubNodes { get; } = new();
+    private bool IsWord { get; set; }
 
-    public Node<T> TryFind(char symbol) // пытается найти дочерний узел (Node<T>) в коллекции SubNodes по заданному символу (symbol)
+    public Node<T> TryFind(char symbol)
     {
         if (SubNodes.TryGetValue(symbol, out Node<T> value))
         {
@@ -22,5 +16,107 @@ public class Node<T>
         {
             return null;
         }
+    }
+    
+    public static void AddNode(string key, Node<T> node)
+    {
+        if (string.IsNullOrEmpty(key))
+        {
+            node.IsWord = true;
+        }
+        else
+        {
+            var symbol = key[0];
+            var subnode = node.TryFind(symbol);
+            if (subnode != null)
+            {
+                AddNode(key[1..], subnode);
+            }
+            else
+            {
+                var newNode = new Node<T>(key[0]);
+                node.SubNodes.Add(key[0], newNode);
+                AddNode(key.Substring(1), newNode);
+            }
+        }
+    }
+    
+    public static bool RemoveNode(string key, Node<T> node)
+    {
+        if (string.IsNullOrEmpty(key))
+        {
+            if (node.IsWord)
+            {
+                node.IsWord = false;
+                return true;
+            }
+            return false;
+        }
+        
+        var subnode = node.TryFind(key[0]);
+        if (subnode == null)
+        {
+            return false;
+        }
+            
+        bool result = RemoveNode(key.Substring(1), subnode);
+
+        if (result)
+        {
+            if (subnode.SubNodes.Count == 0 && !subnode.IsWord)
+            {
+                node.SubNodes.Remove(key[0]);
+            }
+        }
+
+        return result;
+    }
+    
+    public static bool SearchNode(string key, Node<T> node)
+    {
+        var result = false;
+        if (string.IsNullOrEmpty(key))
+        {
+            if (node.IsWord)
+            {
+                result = true;
+                return result;
+            }
+        }
+        else
+        {
+            var subnode = node.TryFind(key[0]);
+            if (subnode != null)
+            {
+                result = SearchNode(key.Substring(1), subnode);
+            }
+        }
+
+        return result;
+    }
+    
+    public static int CountingWordsWithThisPrefix(String prefix, Node<T> node)
+    {
+        int result = 0;
+        while (!string.IsNullOrEmpty(prefix))
+        {
+            node = node.TryFind(prefix[0]);
+            prefix = prefix.Substring(1);
+        }
+        
+        if (node.IsWord)
+        {
+            ++result;
+        }
+
+        if (node.SubNodes.Count > 0)
+        {
+            foreach (var keyValuePair in node.SubNodes)
+            {
+                result += CountingWordsWithThisPrefix(prefix, keyValuePair.Value);
+            }
+        }
+        
+        return result;
     }
 }
