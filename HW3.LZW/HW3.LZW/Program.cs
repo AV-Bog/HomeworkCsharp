09274130787898e640336db4575cc625a22a7019
Bogdanova Arina <arinabogdanova1967@mail.ru>
2025-03-13 23:41:16 +0300

    обработка ввода (оr ее подобие. переделать бы)

--- HW3.LZW/HW3.LZW/Program.cs
+++ HW3.LZW/HW3.LZW/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System.Text;
+using System.IO;
 using HW3.LZW;
 
-byte[] input = Encoding.UTF8.GetBytes("lolololololollalalalabracadabra");
+Console.WriteLine("Введите путь к файлу, который надо сжать или разжать: ");
+string filePath = Console.ReadLine();
+while (!File.Exists(filePath))
+{
+    Console.WriteLine("Нормально введи ПУТЬ ДО ФАЙЛА");
+    filePath = Console.ReadLine();
+}
 
-// Сжатие
-var compressed = LZW.Compress(input);
-Console.WriteLine("Compressed: " + string.Join(", ", compressed));
+Console.WriteLine("Введите ключ -c, означающий, что файл надо сжать, или -u, означающий, что надо разжать: ");
+string userInput = Console.ReadLine();
+if (userInput == "-c")
+{
+    string outputFilePath = GenerateOutputFilePath(filePath, 1);
+    LZW.CompressFile(filePath, outputFilePath);
+}
+if (userInput == "-u")
+{
+    string outputFilePath = GenerateOutputFilePath(filePath, 0);
+    LZW.CompressFile(filePath, outputFilePath);
+}
 
-// Распаковка
-byte[] decompressed = LZW.Decompress(compressed);
-string result = Encoding.UTF8.GetString(decompressed);
-Console.WriteLine("Decompressed: " + result);
+string GenerateOutputFilePath(string filePath, int key)
+{
+    string directory = Path.GetDirectoryName(filePath);
+    string fileName = Path.GetFileNameWithoutExtension(filePath);
+
+    if (key == 1)
+    {
+        return Path.Combine(directory, $"{fileName}.zipped");
+    }
+    else
+    {
+        return Path.Combine(directory, $"{fileName}");
+    }
+} 
