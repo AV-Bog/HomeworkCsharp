7a548c81a19db767479b45d7ef08ba1b7e4d8668
AV-Bog <arinabogdanova1967@mail.ru>
2025-04-04 08:27:51 +0300

    add solution

---
+++ HW5.ParseTree/HW5.ParseTree/HW5.ParseTree/OperationNode.cs
@@ -0,0 +1,69 @@
+namespace HW5.ParseTree;
+
+public abstract class OperationNode : Node
+{
+    protected readonly Node _left;
+    protected readonly Node _right;
+    
+    protected OperationNode(Node left, Node right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public static Node Create(string op, string leftExpr, string rightExpr)
+    {
+        Node left = Parse(leftExpr);
+        Node right = Parse(rightExpr);
+        
+        return op switch
+        {
+            "+" => new AddNode(left, right),
+            "-" => new SubtractNode(left, right),
+            "*" => new MultiplyNode(left, right),
+            "/" => new DivideNode(left, right),
+            _ => throw new FormatException($"Unknown operator: '{op}'")
+        };
+    }
+    public override string Print() => $"({GetOperatorSymbol()} {_left.Print()} {_right.Print()})";
+    protected abstract string GetOperatorSymbol();
+}
+
+public class AddNode : OperationNode
+{
+    public AddNode(Node left, Node right) : base(left, right) {}
+    
+    public override int Evaluate() => _left.Evaluate() + _right.Evaluate();
+    protected override string GetOperatorSymbol() => "+";
+}
+
+public class SubtractNode : OperationNode
+{
+    public SubtractNode(Node left, Node right) : base(left, right) {}
+    
+    public override int Evaluate() => _left.Evaluate() - _right.Evaluate();
+    protected override string GetOperatorSymbol() => "-";
+}
+
+public class MultiplyNode : OperationNode
+{
+    public MultiplyNode(Node left, Node right) : base(left, right) {}
+    
+    public override int Evaluate() => _left.Evaluate() * _right.Evaluate();
+    protected override string GetOperatorSymbol() => "*";
+}
+
+public class DivideNode : OperationNode
+{
+    public DivideNode(Node left, Node right) : base(left, right) {}
+
+    public override int Evaluate()
+    {
+        if (_right == null)
+        {
+            throw new DivideByZeroException();
+        }
+        return _left.Evaluate() / _right.Evaluate();
+    }
+    protected override string GetOperatorSymbol() => "/";
+}
