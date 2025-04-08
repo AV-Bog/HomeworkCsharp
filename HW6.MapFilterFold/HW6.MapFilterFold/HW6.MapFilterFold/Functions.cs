0cc60600b77d7f25e11164207f4404a956f1d69a
AV-Bog <arinabogdanova1967@mail.ru>
2025-04-08 10:29:50 +0300

    add Filter and Fold (тесты надо переимеовать и разможить, комменты наприсать)

--- HW6.MapFilterFold/HW6.MapFilterFold/HW6.MapFilterFold/Functions.cs
+++ HW6.MapFilterFold/HW6.MapFilterFold/HW6.MapFilterFold/Functions.cs
@@ -18,4 +18,41 @@ public class Functions
         }
         return result;
     }
+
+    public static List<TInput> Filter<TInput>(
+        List<TInput> list,
+        Func<TInput, bool> filter)
+    {
+        if (list == null)
+            throw new ArgumentNullException(nameof(list));
+        if (filter == null)
+            throw new ArgumentNullException(nameof(filter));
+    
+        List<TInput> result = new List<TInput>();
+        foreach (var item in list)
+        {
+            if (filter(item))
+            {
+                result.Add(item);
+            }
+        }
+        return result;
+    }
+
+    public static TAcc Fold<TInput, TAcc>(
+        List<TInput> list, 
+        TAcc element, 
+        Func<TAcc, TInput, TAcc> func)
+    {
+        if (list == null) throw new ArgumentNullException(nameof(list));
+        if (func == null) throw new ArgumentNullException(nameof(func));
+        
+        var acc = element;
+        foreach (var item in list)
+        {
+            acc = func(acc, item);
+        }
+        
+        return acc;
+    }
 }
--- HW6.MapFilterFold/HW6.MapFilterFold/TestMapFilterFold/UnitTest1.cs
+++ HW6.MapFilterFold/HW6.MapFilterFold/TestMapFilterFold/UnitTest1.cs
@@ -4,11 +4,35 @@ namespace TestMapFilterFold;
 public class Tests
 {
     [Test]
-    public void Test()
+    public void Test1()
     {
         List<int> list = new List<int> { 1, 2, 3 };
         var rez = Functions.Map(list, x => x * 2);
         
         Assert.That(rez, Is.EqualTo(new List<int> { 2, 4, 6 }));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        List<int> list = new List<int> { 1, 2, 3, 4 };
+        var rez = Functions.Filter(list, x => x % 2 == 0);
+        
+        Assert.That(rez, Is.EqualTo(new List<int> { 2, 4 }));
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        var rez = Functions.Fold(new List<int>() { 1, 2, 3 }, 1, (acc, elem) => acc * elem);
+        
+        Assert.That(rez, Is.EqualTo(6));
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        var rez = Functions.Fold(new List<string>() { "a", "bb", "ccc" }, "D", (acc, elem) => acc + elem);
+        Assert.That(rez, Is.EqualTo(new string ("Dabbccc")));
     }
 }
