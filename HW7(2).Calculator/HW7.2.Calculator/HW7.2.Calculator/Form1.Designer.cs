2186d3ad36a704e87552050754a3bd9fcbba9940
AV-Bog <arinabogdanova1967@mail.ru>
2025-04-25 23:52:42 +0300

    add GI

--- HW7(2).Calculator/HW7.2.Calculator/HW7.2.Calculator/Form1.Designer.cs
+++ HW7(2).Calculator/HW7.2.Calculator/HW7.2.Calculator/Form1.Designer.cs
@@ -24,16 +24,240 @@ partial class Form1
     #region Windows Form Designer generated code
 
     /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        textBox1 = new System.Windows.Forms.TextBox();
+        button1 = new System.Windows.Forms.Button();
+        button3 = new System.Windows.Forms.Button();
+        button2 = new System.Windows.Forms.Button();
+        button5 = new System.Windows.Forms.Button();
+        button6 = new System.Windows.Forms.Button();
+        button4 = new System.Windows.Forms.Button();
+        button8 = new System.Windows.Forms.Button();
+        button9 = new System.Windows.Forms.Button();
+        button7 = new System.Windows.Forms.Button();
+        button10 = new System.Windows.Forms.Button();
+        button11 = new System.Windows.Forms.Button();
+        button12 = new System.Windows.Forms.Button();
+        button13 = new System.Windows.Forms.Button();
+        button14 = new System.Windows.Forms.Button();
+        button15 = new System.Windows.Forms.Button();
+        button16 = new System.Windows.Forms.Button();
+        button17 = new System.Windows.Forms.Button();
+        SuspendLayout();
+        // 
+        // textBox1
+        // 
+        textBox1.Location = new System.Drawing.Point(29, 78);
+        textBox1.Name = "textBox1";
+        textBox1.Size = new System.Drawing.Size(237, 31);
+        textBox1.TabIndex = 0;
+        // 
+        // button1
+        // 
+        button1.Location = new System.Drawing.Point(29, 129);
+        button1.Name = "button1";
+        button1.Size = new System.Drawing.Size(75, 75);
+        button1.TabIndex = 1;
+        button1.Text = "1";
+        button1.UseVisualStyleBackColor = true;
+        // 
+        // button3
+        // 
+        button3.Location = new System.Drawing.Point(191, 129);
+        button3.Name = "button3";
+        button3.Size = new System.Drawing.Size(75, 75);
+        button3.TabIndex = 3;
+        button3.Text = "3";
+        button3.UseVisualStyleBackColor = true;
+        // 
+        // button2
+        // 
+        button2.Location = new System.Drawing.Point(111, 129);
+        button2.Name = "button2";
+        button2.Size = new System.Drawing.Size(75, 75);
+        button2.TabIndex = 2;
+        button2.Text = "2";
+        button2.UseVisualStyleBackColor = true;
+        // 
+        // button5
+        // 
+        button5.Location = new System.Drawing.Point(110, 210);
+        button5.Name = "button5";
+        button5.Size = new System.Drawing.Size(75, 75);
+        button5.TabIndex = 5;
+        button5.Text = "5";
+        button5.UseVisualStyleBackColor = true;
+        // 
+        // button6
+        // 
+        button6.Location = new System.Drawing.Point(191, 210);
+        button6.Name = "button6";
+        button6.Size = new System.Drawing.Size(75, 75);
+        button6.TabIndex = 6;
+        button6.Text = "6";
+        button6.UseVisualStyleBackColor = true;
+        // 
+        // button4
+        // 
+        button4.Location = new System.Drawing.Point(29, 210);
+        button4.Name = "button4";
+        button4.Size = new System.Drawing.Size(75, 75);
+        button4.TabIndex = 4;
+        button4.Text = "4";
+        button4.UseVisualStyleBackColor = true;
+        // 
+        // button8
+        // 
+        button8.Location = new System.Drawing.Point(110, 291);
+        button8.Name = "button8";
+        button8.Size = new System.Drawing.Size(75, 75);
+        button8.TabIndex = 8;
+        button8.Text = "8";
+        button8.UseVisualStyleBackColor = true;
+        // 
+        // button9
+        // 
+        button9.Location = new System.Drawing.Point(191, 291);
+        button9.Name = "button9";
+        button9.Size = new System.Drawing.Size(75, 75);
+        button9.TabIndex = 9;
+        button9.Text = "9";
+        button9.UseVisualStyleBackColor = true;
+        // 
+        // button7
+        // 
+        button7.Location = new System.Drawing.Point(29, 291);
+        button7.Name = "button7";
+        button7.Size = new System.Drawing.Size(75, 75);
+        button7.TabIndex = 7;
+        button7.Text = "7";
+        button7.UseVisualStyleBackColor = true;
+        // 
+        // button10
+        // 
+        button10.Location = new System.Drawing.Point(110, 372);
+        button10.Name = "button10";
+        button10.Size = new System.Drawing.Size(75, 75);
+        button10.TabIndex = 0;
+        button10.Text = "0";
+        button10.UseVisualStyleBackColor = true;
+        // 
+        // button11
+        // 
+        button11.Location = new System.Drawing.Point(191, 372);
+        button11.Name = "button11";
+        button11.Size = new System.Drawing.Size(75, 75);
+        button11.TabIndex = 11;
+        button11.Text = "<<";
+        button11.UseVisualStyleBackColor = true;
+        // 
+        // button12
+        // 
+        button12.Location = new System.Drawing.Point(29, 372);
+        button12.Name = "button12";
+        button12.Size = new System.Drawing.Size(75, 75);
+        button12.TabIndex = 10;
+        button12.Text = "C";
+        button12.UseVisualStyleBackColor = true;
+        // 
+        // button13
+        // 
+        button13.Location = new System.Drawing.Point(287, 78);
+        button13.Name = "button13";
+        button13.Size = new System.Drawing.Size(75, 31);
+        button13.TabIndex = 13;
+        button13.Text = "=";
+        button13.UseVisualStyleBackColor = true;
+        // 
+        // button14
+        // 
+        button14.Location = new System.Drawing.Point(287, 372);
+        button14.Name = "button14";
+        button14.Size = new System.Drawing.Size(75, 75);
+        button14.TabIndex = 17;
+        button14.Text = "/";
+        button14.UseVisualStyleBackColor = true;
+        // 
+        // button15
+        // 
+        button15.Location = new System.Drawing.Point(287, 291);
+        button15.Name = "button15";
+        button15.Size = new System.Drawing.Size(75, 75);
+        button15.TabIndex = 16;
+        button15.Text = "*";
+        button15.UseVisualStyleBackColor = true;
+        // 
+        // button16
+        // 
+        button16.Location = new System.Drawing.Point(287, 210);
+        button16.Name = "button16";
+        button16.Size = new System.Drawing.Size(75, 75);
+        button16.TabIndex = 15;
+        button16.Text = "-";
+        button16.UseVisualStyleBackColor = true;
+        // 
+        // button17
+        // 
+        button17.Location = new System.Drawing.Point(287, 129);
+        button17.Name = "button17";
+        button17.Size = new System.Drawing.Size(75, 75);
+        button17.TabIndex = 14;
+        button17.Text = "+";
+        button17.UseVisualStyleBackColor = true;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(390, 477);
+        Controls.Add(button14);
+        Controls.Add(button15);
+        Controls.Add(button16);
+        Controls.Add(button17);
+        Controls.Add(button13);
+        Controls.Add(button10);
+        Controls.Add(button11);
+        Controls.Add(button12);
+        Controls.Add(button8);
+        Controls.Add(button9);
+        Controls.Add(button7);
+        Controls.Add(button5);
+        Controls.Add(button6);
+        Controls.Add(button4);
+        Controls.Add(button2);
+        Controls.Add(button3);
+        Controls.Add(button1);
+        Controls.Add(textBox1);
+        Text = "Form1";
+        ResumeLayout(false);
+        PerformLayout();
     }
+
+    private System.Windows.Forms.Button button14;
+    private System.Windows.Forms.Button button15;
+    private System.Windows.Forms.Button button16;
+    private System.Windows.Forms.Button button17;
+
+    private System.Windows.Forms.Button button10;
+    private System.Windows.Forms.Button button11;
+    private System.Windows.Forms.Button button12;
+    private System.Windows.Forms.Button button13;
+
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Button button6;
+    private System.Windows.Forms.Button button7;
+    private System.Windows.Forms.Button button8;
+    private System.Windows.Forms.Button button9;
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button1;
 
     #endregion
 }
--- HW7(2).Calculator/HW7.2.Calculator/HW7.2.Calculator/Form1.cs
+++ HW7(2).Calculator/HW7.2.Calculator/HW7.2.Calculator/Form1.cs
@@ -2,8 +2,52 @@ namespace HW7._2.Calculator;
 
 public partial class Form1 : Form
 {
+    private CalculatorEngine calculator = new CalculatorEngine();
+
     public Form1()
     {
         InitializeComponent();
+        InitializeButtonEvents();
+    }
+    
+    private void InitializeButtonEvents()
+    {
+        button1.Click += (s, e) => ProcessInput("1");
+        button2.Click += (s, e) => ProcessInput("2");
+        button3.Click += (s, e) => ProcessInput("3");
+        button4.Click += (s, e) => ProcessInput("4");
+        button5.Click += (s, e) => ProcessInput("5");
+        button6.Click += (s, e) => ProcessInput("6");
+        button7.Click += (s, e) => ProcessInput("7");
+        button8.Click += (s, e) => ProcessInput("8");
+        button9.Click += (s, e) => ProcessInput("9");
+        button10.Click += (s, e) => ProcessInput("0");
+        
+        button17.Click += (s, e) => ProcessInput("+");
+        button16.Click += (s, e) => ProcessInput("-");
+        button15.Click += (s, e) => ProcessInput("*");
+        button14.Click += (s, e) => ProcessInput("/");
+        
+        button13.Click += (s, e) => ProcessInput("=");
+        button12.Click += (s, e) => ProcessInput("C");
+        button11.Click += (s, e) => ProcessInput("<<");
+    }
+
+    private void ProcessInput(string input)
+    {
+        try
+        {
+            calculator.EventHandler(input);
+            UpdateDisplay();
+        }
+        catch (Exception ex)
+        {
+            textBox1.Text = ex.Message;
+        }
+    }
+
+    private void UpdateDisplay()
+    {
+        textBox1.Text = calculator.CurrentValue.ToString();
     }
 }
