f6352b3545ffce265a0bf2e2f9539513bd58e5b6
AV-Bog <arinabogdanova1967@mail.ru>
2025-05-11 17:30:04 +0300

    add solution

---
+++ HW9.MyLinq/MyLinq/TestMyLinq/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="MyLinq.cs" author="AV-Bog">
+// under MIT License
+// </copyright>
+
+using HW9.MyLinq;
+
+namespace TestMyLinq;
+
+public class Tests
+{
+    [Test]
+    public void GetPrimes_FirstEightPrimes_AreCorrect()
+    {
+        var expectedPrimes = new[] { 2, 3, 5, 7, 11, 13, 17, 19 };
+        var primes = MyLinq.GetPrimes().TakeFirst(8);
+        Assert.That(primes, Is.EqualTo(expectedPrimes));
+    }
+    
+    [Test]
+    public void TakeFirst_ReturnsCorrectNumberOfElements()
+    {
+        var source = new[] { 1, 2, 3, 4, 5 };
+        var result = source.TakeFirst(2).ToList();
+
+        Assert.Equals(new[] { 1, 2 }, result);
+    }
+
+    [Test]
+    public void TakeFirst_WhenFewerElements_ReturnsAllAvailable()
+    {
+        var source = new[] { 1, 2 };
+        var result = source.TakeFirst(5).ToList();
+
+        Assert.Equals(new[] { 1, 2 }, result);
+    }
+    
+    [Test]
+    public void MySkip_SkipsCorrectNumberOfElements()
+    {
+        var source = new[] { 1, 2, 3, 4, 5 };
+        var result = source.MySkip(2).ToList();
+
+        Assert.Equals(new[] { 3, 4, 5 }, result);
+    }
+
+    [Test]
+    public void MySkip_WhenSkipZero_ReturnsFullSequence()
+    {
+        var source = new[] { 1, 2, 3 };
+        var result = source.MySkip(0).ToList();
+
+        Assert.Equals(source, result);
+    }
+
+    [Test]
+    public void GetPrimes_IsLazy_DoesNotHangOnInfiniteSequence()
+    {
+        var prime = MyLinq.GetPrimes().Skip(100).TakeFirst(1).Single();
+        Assert.Equals(MyLinq.IsPrime(prime), true);
+    }
+}
